6e9c04a2a86d8b679c407f8bd3bbfa3b47616b32
Zethembe Madlala <143174322+FaniZH@users.noreply.github.com>
2024-09-23 08:43:38 +0200

    Add files via upload

--- MutCampusApp/src/app/faculties/faculties.page.ts
+++ MutCampusApp/src/app/faculties/faculties.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faculties',
   templateUrl: './faculties.page.html',
   styleUrls: ['./faculties.page.scss'],
 })
-export class FacultiesPage implements OnInit {
+export class FacultiesPage {
 
-  constructor() { }
+  // State for managing which faculties are expanded
+  showMoreState: { [key: string]: boolean } = {
+    engineering: false,
+    management: false,
+    naturalSciences: false
+  };
 
-  ngOnInit() {
+  // Toggle the state for each faculty
+  toggleShowMore(event: Event, faculty: string) {
+    event.preventDefault();  // Prevent the default anchor behavior
+    this.showMoreState[faculty] = !this.showMoreState[faculty];
   }
-
 }
